cb6bbcfe13c4d7f366a915bbd0935aec64b62371
Heungsub Lee <sub@subl.ee>
2017-06-05 16:34:27 +0900

    Introduce ViewResultBase
    It shares similar code between ViewResult and N1QLResult.

--- couchbase_ffi/n1ql.py
+++ couchbase_ffi/n1ql.py
@@ -1,9 +1,7 @@
 from couchbase_ffi._cinit import get_handle
 from couchbase_ffi._rtconfig import pycbc_exc_lcb, PyCBC
-from couchbase_ffi._strutil import from_cstring
 from couchbase_ffi.bufmanager import BufManager
-from couchbase_ffi.result import Result
-
+from couchbase_ffi.view import buf2str, ViewResultBase
 
 ffi, C = get_handle()
 
@@ -46,123 +44,30 @@ class _N1QLParams(object):
         C.lcb_n1p_reset(self._lp)
 
 
-ROWCB_DECL = 'void(lcb_t,int,const lcb_RESPN1QL*)'
+class N1QLResult(ViewResultBase):
+    COMMAND_DECL = 'lcb_CMDN1QL*'
+    HANDLE_DECL = 'lcb_N1QLHANDLE*'
+    ROWCB_DECL = 'void(lcb_t,int,const lcb_RESPN1QL*)'
 
-
-def buf2str(v, n):
-    return from_cstring(ffi.cast('const char*', v), n)
-
-
-class N1QLResult(Result):
     def __init__(self, params, prepare=0, cross_bucket=0):
-        self._c_command = ffi.new('lcb_CMDN1QL*')
-        self._c_handle = ffi.new('lcb_N1QLHANDLE*')
+        super(N1QLResult, self).__init__()
         self._params = params
         self._prepare = prepare
         self._cross_bucket = cross_bucket
-        self._parent = None
-        self.rows = []
 
-        # self._rows_per_call = 0
-        self._bound_cb = ffi.callback(ROWCB_DECL, self._on_single_row)
-        self.done = False
-        self.value = None
-        self.http_status = 0
-
-    @property
-    def key(self):
-        return 'VIEW[{0}/{1}]'.format(self._ddoc, self._view)
-
-    @property
-    def rows_per_call(self):
-        return self._rows_per_call
-    @rows_per_call.setter
-    def rows_per_call(self, val):
-        self._rows_per_call = int(val)
-
-    def _schedule(self, parent, mres):
+    def _init_command(self):
         bm = BufManager(ffi)
-        urlopts = ffi.NULL
-        pypost = None
         cmd = self._c_command
-
         C._Cb_n1ql_query_initcmd(cmd, bm.new_cstr(self._params),
                                  len(self._params), self._bound_cb)
-
-        # if pypost:
-        #     cmd.postdata, cmd.npostdata = bm.new_cbuf(pypost)
-
         cmd.handle = self._c_handle
 
-        self._parent = parent
-        rc = C.lcb_n1ql_query(parent._lcbh, mres._cdata, self._c_command)
-        if rc:
-            raise pycbc_exc_lcb(rc)
+    def _query(self, parent, mres):
+        return C.lcb_n1ql_query(parent._lcbh, mres._cdata, self._c_command)
 
-    def _handle_done(self, resp, mres):
-        self._c_handle = None
-        if resp.rc:
-            if resp.rc == C.LCB_HTTP_ERROR:
-                try:
-                    raise PyCBC.exc_http(self.value)
-                except:
-                    mres._add_err(sys.exc_info())
-            else:
-                mres._add_bad_rc(resp.rc, self)
+    def _handle_resp(self, resp, mres):
+        pass
 
-        if resp.htresp:
-            if not self.value and resp.htresp.nbody:
-                self.value = buf2str(resp.htresp.body, resp.htresp.nbody)
-            self.http_status = resp.htresp.htstatus
-
-        if self._parent._is_async:
-            self._invoke_async(mres, is_final=True)
-
-        self.done = True
-
-        if self._parent._is_async:
-            try:
-                mres._maybe_throw()
-                self._invoke_async(mres, is_final=True)
-            except:
-                mres.errback(mres, *sys.exc_info())
-            finally:
-                del self._parent
-
-    def _should_call(self, is_final):
-        if is_final:
-            return True
-        return -1 < self._rows_per_call < len(self.rows)
-
-    def _invoke_async(self, mres, is_final=False):
-        if not self._should_call(is_final=is_final):
-            return
-
-        cb = mres.callback
-        if cb:
-            cb(mres)
-            self.rows = []
-
-    def _on_single_row(self, instance, cbtype, resp):
-        mres = ffi.from_handle(resp.cookie)
-        if resp.rflags & C.LCB_RESP_F_FINAL:
-            self._handle_done(resp, mres)
-            return
-
-        if resp.rc != C.LCB_SUCCESS:
-            mres._add_bad_rc(resp.rc, self)
-            return
-
+    def _process_resp(self, resp, mres):
         if resp.nrow:
-            row = PyCBC.json_decode(buf2str(resp.row, resp.nrow))
-            # So now that we have a row..
-            self.rows.append(row)
-        if self._parent._is_async:
-            self._invoke_async(mres)
-
-    def fetch(self, mres):
-        C.lcb_wait(self._parent._lcbh)
-        ret = self.rows
-        self.rows = []
-        mres._maybe_throw()
-        return ret
+            return PyCBC.json_decode(buf2str(resp.row, resp.nrow))
--- couchbase_ffi/view.py
+++ couchbase_ffi/view.py
@@ -8,8 +8,6 @@ from couchbase_ffi._strutil import from_cstring
 
 ffi, C = get_handle()
 
-ROWCB_DECL = 'void(lcb_t,int,const lcb_RESPVIEWQUERY*)'
-
 
 def mres2vres(mres):
     return mres[None]
@@ -19,61 +17,35 @@ def buf2str(v, n):
     return from_cstring(ffi.cast('const char*', v), n)
 
 
-class ViewResult(Result):
-    def __init__(self, ddoc, view, options, flags):
-        self._c_command = ffi.new('lcb_CMDVIEWQUERY*')
-        self._c_handle = ffi.new('lcb_VIEWHANDLE*')
-        self._ddoc = ddoc
-        self._view = view
-        self._options = options
-        self._flags = flags
+class ViewResultBase(Result):
+    COMMAND_DECL  = NotImplemented
+    HANDLE_DECL = NotImplemented
+    ROWCB_DECL = NotImplemented
+
+    def __init__(self):
+        self._c_command = ffi.new(self.COMMAND_DECL)
+        self._c_handle = ffi.new(self.HANDLE_DECL)
         self._parent = None
         self.rows = []
         self._rows_per_call = 0
-        self._bound_cb = ffi.callback(ROWCB_DECL, self._on_single_row)
+        self._bound_cb = ffi.callback(self.ROWCB_DECL, self._on_single_row)
         self.done = False
         self.value = None
         self.http_status = 0
 
-    @property
-    def key(self):
-        return 'VIEW[{0}/{1}]'.format(self._ddoc, self._view)
-
-    @property
-    def rows_per_call(self):
-        return self._rows_per_call
-    @rows_per_call.setter
-    def rows_per_call(self, val):
-        self._rows_per_call = int(val)
-
     def _schedule(self, parent, mres):
-        bm = BufManager(ffi)
-        urlopts = ffi.NULL
-        pypost = None
-        cmd = self._c_command
-
-        if self._options:
-            in_uri, in_post = self._options._long_query_encoded
-            # Note, encoded means URI/JSON encoded; not charset
-            urlopts = bm.new_cstr(in_uri)
-            if in_post and in_post != '{}':
-                pypost = in_post
-
-        C.lcb_view_query_initcmd(
-            cmd, bm.new_cstr(self._ddoc), bm.new_cstr(self._view),
-            urlopts, self._bound_cb)
-
-        if pypost:
-            cmd.postdata, cmd.npostdata = bm.new_cbuf(pypost)
-
-        cmd.cmdflags = self._flags
-
-        self._c_command.handle = self._c_handle
-
+        self._init_command()
         self._parent = parent
-        rc = C.lcb_view_query(parent._lcbh, mres._cdata, self._c_command)
+        rc = self._query(parent, mres)
         if rc:
             raise pycbc_exc_lcb(rc)
+
+    def _init_command(self):
+        raise NotImplementedError
+
+    def _query(self, parent, mres):
+        # should return rc
+        raise NotImplementedError
 
     def _handle_done(self, resp, mres):
         self._c_handle = None
@@ -86,12 +58,7 @@ class ViewResult(Result):
             else:
                 mres._add_bad_rc(resp.rc, self)
 
-        if resp.nvalue:
-            self.value = buf2str(resp.value, resp.nvalue)
-            try:
-                self.value = PyCBC.json_decode(self.value)
-            except:
-                pass
+        self._handle_resp(resp, mres)
 
         if resp.htresp:
             if not self.value and resp.htresp.nbody:
@@ -111,6 +78,16 @@ class ViewResult(Result):
                 mres.errback(mres, *sys.exc_info())
             finally:
                 del self._parent
+
+    def _handle_resp(self, resp, mres):
+        raise NotImplementedError
+
+    @property
+    def rows_per_call(self):
+        return self._rows_per_call
+    @rows_per_call.setter
+    def rows_per_call(self, val):
+        self._rows_per_call = int(val)
 
     def _should_call(self, is_final):
         if is_final:
@@ -142,6 +119,78 @@ class ViewResult(Result):
             mres._add_bad_rc(resp.rc, self)
             return
 
+        row = self._process_resp(resp, mres)
+
+        if row is not None:
+            # So now that we have a row..
+            self.rows.append(row)
+
+        if self._parent._is_async:
+            self._invoke_async(mres)
+
+    def _process_resp(self, resp, mres):
+        raise NotImplementedError
+
+    def fetch(self, mres):
+        C.lcb_wait(self._parent._lcbh)
+        ret = self.rows
+        self.rows = []
+        mres._maybe_throw()
+        return ret
+
+
+class ViewResult(ViewResultBase):
+    COMMAND_DECL = 'lcb_CMDVIEWQUERY*'
+    HANDLE_DECL = 'lcb_VIEWHANDLE*'
+    ROWCB_DECL = 'void(lcb_t,int,const lcb_RESPVIEWQUERY*)'
+
+    def __init__(self, ddoc, view, options, flags):
+        super(ViewResult, self).__init__()
+        self._ddoc = ddoc
+        self._view = view
+        self._options = options
+        self._flags = flags
+
+    @property
+    def key(self):
+        return 'VIEW[{0}/{1}]'.format(self._ddoc, self._view)
+
+    def _init_command(self):
+        cmd = self._c_command
+
+        bm = BufManager(ffi)
+        urlopts = ffi.NULL
+        pypost = None
+
+        if self._options:
+            in_uri, in_post = self._options._long_query_encoded
+            # Note, encoded means URI/JSON encoded; not charset
+            urlopts = bm.new_cstr(in_uri)
+            if in_post and in_post != '{}':
+                pypost = in_post
+
+        C.lcb_view_query_initcmd(
+            cmd, bm.new_cstr(self._ddoc),
+            bm.new_cstr(self._view), urlopts, self._bound_cb)
+
+        if pypost:
+            cmd.postdata, cmd.npostdata = bm.new_cbuf(pypost)
+
+        cmd.cmdflags = self._flags
+        cmd.handle = self._c_handle
+
+    def _query(self, parent, mres):
+        return C.lcb_view_query(parent._lcbh, mres._cdata, self._c_command)
+
+    def _handle_resp(self, resp, mres):
+        if resp.nvalue:
+            self.value = buf2str(resp.value, resp.nvalue)
+            try:
+                self.value = PyCBC.json_decode(self.value)
+            except:
+                pass
+
+    def _process_resp(self, resp, mres):
         row = {}
         if resp.nkey:
             row['key'] = PyCBC.json_decode(buf2str(resp.key, resp.nkey))
@@ -150,7 +199,6 @@ class ViewResult(Result):
         if resp.docid:
             # Document ID is always a simple string, so no need to decode
             row['id'] = buf2str(resp.docid, resp.ndocid)
-
         if resp.docresp:
             py_doc = ValueResult()
             l_doc = resp.docresp
@@ -166,17 +214,4 @@ class ViewResult(Result):
                     py_doc.value = tc.decode_value(buf, py_doc.flags)
                 except:
                     py_doc.value = buf[::]
-
-        # So now that we have a row..
-        self.rows.append(row)
-        if self._parent._is_async:
-            self._invoke_async(mres)
-
-
-    def fetch(self, mres):
-        C.lcb_wait(self._parent._lcbh)
-        ret = self.rows
-        self.rows = []
-        mres._maybe_throw()
-        return ret
-
+        return row
